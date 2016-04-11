8511b12ffe994d117964704e30bb45afb2196ca6
OArkbird <adegunoy@gmail.com>
2016-04-11 17:46:43 +0100

    Ok

--- PaysVilles/AlphaChecker.cs
+++ PaysVilles/AlphaChecker.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace PaysVilles
 {
     public class AlphaChecker
     {
-
+        Regex alphabet = new Regex("[a-z,A-Z]");
+ 
        virtual public bool IsALetter(char letter)
         {
-            throw new NotImplementedException();
+            if (alphabet.IsMatch(letter.ToString()))
+           {
+               return true;
+           }
+            else 
+            {
+                return false;
+            }
+            
         }
     }
 }
---
+++ PaysVillesTest/AlphaCheckerTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaysVilles;
+
+namespace PaysVillesTest
+{
+    [TestClass]
+    public class AlphaCheckerTest
+    {
+        [TestMethod]
+        public void Test_AlphaChecher_ReturnsTrue_WhenIsALetterIsCalled_GivenAThecharAInArgument()
+        {
+            //Arrange
+            AlphaChecker alphaCheck = new AlphaChecker();
+            //Act
+
+            bool testValue = alphaCheck.IsALetter('A');
+            //Assert
+            Assert.AreEqual(true, testValue);
+        }
+
+        [TestMethod]
+        public void Test_AlphaChecher_ReturnsFalse_WhenIsALetterIsCalled_GivenAThechar9InArgument()
+        {
+            //Arrange
+            AlphaChecker alphaCheck = new AlphaChecker();
+            //Act
+
+            bool testValue = alphaCheck.IsALetter('9');
+            //Assert
+            Assert.AreEqual(false, testValue);
+        }
+    }
+}
