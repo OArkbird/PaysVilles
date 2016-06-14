b82069990710af9a72673e0e78cf2b68d68d76df
Oarkbird <adegunoy@gmail.com>
2016-06-14 09:59:51 +0100

    TextFile database added

---
+++ PVRepositoriesTest/WordRepoTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PVRepositoriesTest
+{
+    [TestClass]
+    public class WordRepoTest
+    {
+        [TestMethod]
+        public void Test_WordRepo_LoadsAllTheWordsFromTheTextFileToHisWordList_WhenLoadMethodIsCalled()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+    }
+}
